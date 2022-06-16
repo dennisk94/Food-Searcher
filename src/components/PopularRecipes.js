89229cba3576ece52f23dcc48fd998663021b88a
Dennis Kim <dennis.kim@hotmail.com>
2022-06-15 18:52:03 -0700

    add responsive styling to popular recipes component

--- src/components/PopularRecipes.js
+++ src/components/PopularRecipes.js
@@ -8,30 +8,32 @@ const PopularRecipes = () => {
       <h2>
         Popular
       </h2>
-      <div className="popular__card">
-        <Link to='/'>
-          <img src={ imgPlaceholder } alt="placeholder" />
-        </Link>
-        <div className="popular__cta">
-          <RecipeCta />
+      <div className="popular__cards-wrapper">
+        <div className="popular__card">
+          <Link to='/'>
+            <img src={ imgPlaceholder } alt="placeholder" />
+          </Link>
+          <div className="popular__cta">
+            <RecipeCta />
+          </div>
         </div>
-      </div>
 
-      <div className="popular__card">
-        <Link to='/'>
-          <img src={ imgPlaceholder } alt="placeholder" />
-        </Link>
-        <div className="popular__cta">
-          <RecipeCta />
+        <div className="popular__card">
+          <Link to='/'>
+            <img src={ imgPlaceholder } alt="placeholder" />
+          </Link>
+          <div className="popular__cta">
+            <RecipeCta />
+          </div>
         </div>
-      </div>
 
-      <div className="popular__card">
-        <Link to='/'>
-          <img src={ imgPlaceholder } alt="placeholder" />
-        </Link>
-        <div className="popular__cta">
-          <RecipeCta />
+        <div className="popular__card">
+          <Link to='/'>
+            <img src={ imgPlaceholder } alt="placeholder" />
+          </Link>
+          <div className="popular__cta">
+            <RecipeCta />
+          </div>
         </div>
       </div>
     </div>
