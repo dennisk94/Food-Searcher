1159c7eb51ba0d3bd625c600303bb83b4d5f5db8
Dennis Kim <dennis.kim@hotmail.com>
2022-06-15 18:39:21 -0700

    add popular recipe section

---
+++ src/components/PopularRecipes.js
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom';
+import imgPlaceholder from '../img/banner-placeholder.jpg';
+import RecipeCta from './RecipeCta';
+
+const PopularRecipes = () => {
+  return (
+    <div className="popular">
+      <h2>
+        Popular
+      </h2>
+      <div className="popular__card">
+        <Link to='/'>
+          <img src={ imgPlaceholder } alt="placeholder" />
+        </Link>
+        <div className="popular__cta">
+          <RecipeCta />
+        </div>
+      </div>
+
+      <div className="popular__card">
+        <Link to='/'>
+          <img src={ imgPlaceholder } alt="placeholder" />
+        </Link>
+        <div className="popular__cta">
+          <RecipeCta />
+        </div>
+      </div>
+
+      <div className="popular__card">
+        <Link to='/'>
+          <img src={ imgPlaceholder } alt="placeholder" />
+        </Link>
+        <div className="popular__cta">
+          <RecipeCta />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PopularRecipes
