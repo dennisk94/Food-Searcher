2149ffd8f72c562c05ba4d31735e744f4a857ed1
Dennis Kim <dennis.kim@hotmail.com>
2022-06-15 21:57:04 -0700

    add category component

---
+++ src/components/Category.js
@@ -0,0 +1,16 @@
+import RecipeCard from "./RecipeCard";
+import ViewMore from "./ViewMore";
+
+const Category = () => {
+  return (
+    <div className="category">
+      <h2>
+        Baking
+      </h2>
+      <RecipeCard />
+      <ViewMore />
+    </div>
+  )
+}
+
+export default Category;
