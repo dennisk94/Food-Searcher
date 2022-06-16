81efccbb78a948e55bc0919d169e0257bfe66153
Dennis Kim <dennis.kim@hotmail.com>
2022-06-15 21:18:34 -0700

    add cuisine component and styling

---
+++ src/components/Cuisine.js
@@ -0,0 +1,14 @@
+import RecipeCard from "./RecipeCard";
+
+const Cuisine = () => {
+  return (
+    <div className="cuisine">
+      <h2>
+        Italian
+      </h2>
+      <RecipeCard />
+    </div>
+  )
+}
+
+export default Cuisine;
