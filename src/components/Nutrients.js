56a0a9fa3c722939f34e95d3aad0099cf4a9903a
Dennis Kim <dennis.kim@hotmail.com>
2022-06-15 21:57:28 -0700

    add nutrients component

---
+++ src/components/Nutrients.js
@@ -0,0 +1,16 @@
+import RecipeCard from "./RecipeCard";
+import ViewMore from "./ViewMore";
+
+const Nutrients = () => {
+  return (
+    <div className="nutrients">
+      <h2>
+        Vegan
+      </h2>
+      <RecipeCard />
+      <ViewMore />
+    </div>
+  )
+}
+
+export default Nutrients;
