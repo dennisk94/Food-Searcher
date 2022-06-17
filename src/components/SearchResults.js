d004acbd1eb8509f5ff3767deeaa5fae83f5c1f5
Dennis Kim <dennis.kim@hotmail.com>
2022-06-16 17:25:32 -0700

    add search results component and styling

---
+++ src/components/SearchResults.js
@@ -0,0 +1,14 @@
+import RecipeCard from "./RecipeCard";
+
+const SearchResults = () => {
+  return (
+    <div className="results">
+      <h2>
+        Results
+      </h2>
+      <RecipeCard />
+    </div>
+  )
+}
+
+export default SearchResults
