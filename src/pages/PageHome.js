c0dffa9b7258b369e9826f38d058181aea7ae8f7
Dennis Kim <dennis.kim@hotmail.com>
2022-06-16 18:44:10 -0700

    add api call code block

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import { API_KEY } from '../globals/apiKey';
 import Banner from "../components/Banner";
 import Popular from "../components/Popular";
 import Search from "../components/Search";
@@ -8,6 +10,22 @@ import SearchByIngredients from "../components/SearchByIngredients";
 import SearchResults from "../components/SearchResults";
 
 const PageHome = () => {
+
+  const [ recipeData, setRecipeData ] = useState(null);
+
+  useEffect( () => {
+    // Get popular recipes
+    const fetchPopularRecipes = async () => {
+      const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?sort=popularity&apiKey=${ API_KEY }`);
+      let rawRecipeData = await res.json();
+      // rawRecipeData = rawRecipeData.results.splice(0, 1);
+      setRecipeData(rawRecipeData.results);
+      console.log(rawRecipeData.results);
+    };
+
+    fetchPopularRecipes();
+  }, []);
+
   return (
     <div className="page-home">
       <Banner />
@@ -23,4 +41,4 @@ const PageHome = () => {
   )
 }
 
-export default PageHome
+export default PageHome;
