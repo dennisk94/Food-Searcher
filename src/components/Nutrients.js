cbc3228fd3c077c2e2768d747cf89b73930b1d10
Dennis Kim <dennis.kim@hotmail.com>
2022-06-18 15:31:33 -0700

    add dynamic info for nutrients component

--- src/components/Nutrients.js
+++ src/components/Nutrients.js
@@ -1,13 +1,19 @@
 import RecipeCard from "./RecipeCard";
 import ViewMore from "./ViewMore";
 
-const Nutrients = () => {
+const Nutrients = ( { veganRecipes } ) => {
   return (
     <div className="nutrients">
       <h2>
         Vegan
       </h2>
-      <RecipeCard />
+      <div className="cards-wrapper">
+        {
+          veganRecipes.map( ( recipe ) => 
+          <RecipeCard key={ recipe.id } id={ recipe.id } title={recipe.title} img={recipe.image} type={ recipe.imageType }/>
+          )
+        }
+      </div>
       <ViewMore />
     </div>
   )
