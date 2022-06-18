5f139ab89edd75bf703df68ad0d986168c85bd46
Dennis Kim <dennis.kim@hotmail.com>
2022-06-18 15:33:14 -0700

    add unfinished search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,38 @@
+import { useRef } from "react";
 import { GoSearch } from "react-icons/go";
 
 const Search = () => {
+
+  let searchRef = useRef();
+  
+  const searchInputHandler = (e) => {
+    e.preventDefault();
+    const inputValue = searchRef.current.value;
+    console.log(inputValue);
+    // searchRef = '';
+  };
+
   return (
     <div className="search">
         <h2 className="search__heading">
             Search
         </h2>
+        <form onSubmit={searchInputHandler}>
+
         <div className="search__input-wrapper">
-            <input type="text" placeholder="Search for recipe by name..." className="search__input" />
-            <GoSearch className="search__icon"/>
+            <input 
+              type="text" 
+              placeholder="Search for recipe by name..." 
+              className="search__input" 
+              ref={ searchRef }
+              />
+            <GoSearch 
+              className="search__icon"
+              onClick={ searchInputHandler }
+              />
         </div>
+        </form>
+
     </div>
   )
 }
