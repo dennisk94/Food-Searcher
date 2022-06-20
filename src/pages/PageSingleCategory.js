18c94dc3f6ba5466f00252c1633dcc003f94211f
Dennis Kim <dennis.kim@hotmail.com>
2022-06-19 17:53:29 -0700

    add single category page

---
+++ src/pages/PageSingleCategory.js
@@ -0,0 +1,12 @@
+
+const PageSingleCategory = () => {
+    return (
+      <div className="single-category">
+          <h1>
+              Single Category
+          </h1>
+      </div>
+    )
+  }
+  
+  export default PageSingleCategory;
