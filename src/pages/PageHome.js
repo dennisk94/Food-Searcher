f9285ee12631f2ea983f4526bad1c6db068e783c
Dennis Kim <dennis.kim@hotmail.com>
2022-06-19 17:55:39 -0700

    comment out fetch code to avoid hitting max API requests

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -8,6 +8,7 @@ import Dessert from '../components/Dessert';
 import Nutrients from "../components/Nutrients";
 import SearchByIngredients from "../components/SearchByIngredients";
 import SearchResults from "../components/SearchResults";
+import SearchResultsByIngredients from '../components/SearchResultsByIngredients';
 
 const PageHome = () => {
 
@@ -15,73 +16,104 @@ const PageHome = () => {
   const [ cuisineRecipeData, setCuisineRecipeData ] = useState(null);
   const [ dessertRecipeData, setDessertRecipeData ] = useState(null);
   const [ veganRecipeData, setVeganRecipeData ] = useState(null);
+  const [ searchRecipeData, setSearchRecipeData ] = useState(null);
+  const [ searchTerm, setSearchTerm ] = useState(null);
+  const [ recipeByIngredients, setRecipeByIngredientsData ] = useState(null);
+  const [ filterTerms, setFilterTerm ] = useState( [] );
 
   useEffect( () => {
     // Get popular recipes
-    const fetchPopularRecipes = async () => {
-      const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?sort=popularity&apiKey=${ API_KEY }`);
-      let rawRecipeData = await res.json();
-      rawRecipeData = rawRecipeData.results.splice(0, 3);
-      setPopularRecipeData(rawRecipeData);
-      // console.log(rawRecipeData);
-    };
+  //   const fetchPopularRecipes = async () => {
+  //     const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?sort=popularity&apiKey=${ API_KEY }`);
+  //     let rawRecipeData = await res.json();
+  //     rawRecipeData = rawRecipeData.results.splice(0, 3);
+  //     setPopularRecipeData(rawRecipeData);
+  //     // console.log(rawRecipeData);
+  //   };
 
-    fetchPopularRecipes();
+  //   fetchPopularRecipes();
 
-   // Get cuisine recipes
-    const fetchCuisineRecipes = async () => {
-    const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?sort=popularity&cuisine=Italian&apiKey=${ API_KEY }`);
-    let rawRecipeData = await res.json();
-    rawRecipeData = rawRecipeData.results.splice(0, 3);
-    setCuisineRecipeData(rawRecipeData);
-    // console.log(rawRecipeData);
-  };
+  //  // Get cuisine recipes
+  //   const fetchCuisineRecipes = async () => {
+  //   const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?sort=popularity&cuisine=Italian&apiKey=${ API_KEY }`);
+  //   let rawRecipeData = await res.json();
+  //   rawRecipeData = rawRecipeData.results.splice(0, 3);
+  //   setCuisineRecipeData(rawRecipeData);
+  //   // console.log(rawRecipeData);
+  // };
 
-  fetchCuisineRecipes();
+  // fetchCuisineRecipes();
 
-  // Get cuisine recipes
-  const fetchBeverageRecipes = async () => {
-    const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?type=dessert&sort=popularity&apiKey=${ API_KEY }`);
-    let rawRecipeData = await res.json();
-    rawRecipeData = rawRecipeData.results.splice(0, 3);
-    setDessertRecipeData(rawRecipeData);
-    // console.log(rawRecipeData);
-  };
+  // // Get dessert recipes
+  // const fetchBeverageRecipes = async () => {
+  //   const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?type=dessert&sort=popularity&apiKey=${ API_KEY }`);
+  //   let rawRecipeData = await res.json();
+  //   rawRecipeData = rawRecipeData.results.splice(0, 3);
+  //   setDessertRecipeData(rawRecipeData);
+  //   // console.log(rawRecipeData);
+  // };
 
-  fetchBeverageRecipes();
+  // fetchBeverageRecipes();
 
-  // Get cuisine recipes
-  const fetchVeganRecipes = async () => {
-    const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?diet=Vegan&sort=popularity&apiKey=${ API_KEY }`);
-    let rawRecipeData = await res.json();
-    rawRecipeData = rawRecipeData.results.splice(0, 3);
-    setVeganRecipeData(rawRecipeData);
-    // console.log(rawRecipeData);
-  };
+  // // Get vegan recipes
+  // const fetchVeganRecipes = async () => {
+  //   const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?diet=Vegan&sort=popularity&apiKey=${ API_KEY }`);
+  //   let rawRecipeData = await res.json();
+  //   rawRecipeData = rawRecipeData.results.splice(0, 3);
+  //   setVeganRecipeData(rawRecipeData);
+  //   // console.log(rawRecipeData);
+  // };
 
-  fetchVeganRecipes();
+  // fetchVeganRecipes();
 
-  }, []);
+   // Get searched recipes
+    if ( searchTerm ) {
+      const fetchSearchedRecipes = async () => {
+        const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${ searchTerm }&sort=popularity&apiKey=${ API_KEY }`);
+        let rawRecipeData = await res.json();
+        rawRecipeData = rawRecipeData.results.splice(0, 3);
+        setSearchRecipeData(rawRecipeData);
+        // console.log(rawRecipeData);
+      };
+    
+      fetchSearchedRecipes();
+    }
 
-  if ( !popularRecipeData   ||
-       !cuisineRecipeData   ||
-       !dessertRecipeData  ||
-       !veganRecipeData
-    ){
-    return;
-  }
+    // Get searched recipes based on ingredients
+    // if ( filterTerms ) {
+    //   console.log(filterTerms);
+    //   const fetchRecipesByIngredients = async () => {
+    //     const res = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ filterTerms[0] },${ filterTerms.slice(1).map( ( term ) => `+${term},`) }&apiKey=${ API_KEY }`);
+    //     let rawRecipeData = await res.json();
+    //     rawRecipeData = rawRecipeData.splice(0, 3);
+    //     console.log(rawRecipeData);
+    //     setRecipeByIngredientsData(rawRecipeData);
+    //   };
+    
+    //   fetchRecipesByIngredients();
+    // }
+
+  }, [searchTerm, filterTerms]);
+
+  // if ( !popularRecipeData   ||
+  //      !cuisineRecipeData   ||
+  //      !dessertRecipeData   ||
+  //      !veganRecipeData     
+  //   ){
+  //   return;
+  // }
 
   return (
     <div className="page-home">
       <Banner />
       <Popular popularRecipes={ popularRecipeData }/>
-      <Search />
-      <SearchResults />
+      <Search setSearchTerm={ setSearchTerm }/>
+      <SearchResults searchRecipes={ searchRecipeData }/>
       <Cuisine cuisineRecipes={ cuisineRecipeData }/>
       <Dessert dessertRecipes={ dessertRecipeData }/>
       <Nutrients veganRecipes={ veganRecipeData }/>
-      <SearchByIngredients />
-      <SearchResults />
+      <SearchByIngredients setFilterTerm={ setFilterTerm } filterTerms={ filterTerms }/>
+      <SearchResultsByIngredients recipeByIngredients={ recipeByIngredients }/>
     </div>
   )
 }
