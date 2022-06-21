0c0f7ed8eead58733d633c3957364e5a1fdefff0
Dennis Kim <dennis.kim@hotmail.com>
2022-06-20 22:33:20 -0700

    change banner layout

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,17 @@
 import placeHolder from '../img/banner-placeholder.jpg';
 import { Link } from 'react-router-dom';
+import RecipeCard from './RecipeCard';
 
-const Banner = () => {
+const Banner = ( { randomRecipes } ) => {
   return (
     <div className="banner">
-        <img src={ placeHolder } alt="Banner" className='banner__img'/>
+        {
+          randomRecipes && randomRecipes.map( ( recipe, i ) => <RecipeCard key={ recipe.id } 
+                                                                          id={ recipe.id } 
+                                                                          title={recipe.title} 
+                                                                          type={ recipe.imageType }/>)
+        }
+        {/* <img src={ placeHolder } alt="Banner" className='banner__img'/>
         <div className="banner__overlay"></div>
         <div className="banner__text">
           <h2 className='banner__heading'>
@@ -14,7 +21,7 @@ const Banner = () => {
           </h2>
         
           <Link to='/' className='banner__link'>View Recipe</Link>
-        </div>
+        </div> */}
     </div>
   )
 }
