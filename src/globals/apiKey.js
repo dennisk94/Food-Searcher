b98c2c61dfe83fcae351b7ea7828f550af4a8f9c
Dennis Kim <dennis.kim@hotmail.com>
2022-06-21 18:45:49 -0700

    add logo and custom font

--- src/globals/apiKey.js
+++ src/globals/apiKey.js
@@ -1,4 +1,4 @@
 // export const API_KEY = '675314a2f0b54d7c83ff83ebf409fea4'; // dennis.kim@hotmail.com
 // export const API_KEY = '347319ca970f403bbca7249f58f33b62'; // dennis.kim132456@gmail.com
-// export const API_KEY = '8d5ba2b7732e4b2b869f525783d3ede2'; // spoonacular123@gmail.com
-export const API_KEY = 'c0391eb6c1ce416eaf939536ef899bcf'; // spoonacular1234@gmail.com
+export const API_KEY = '8d5ba2b7732e4b2b869f525783d3ede2'; // spoonacular123@gmail.com
+// export const API_KEY = 'c0391eb6c1ce416eaf939536ef899bcf'; // spoonacular1234@gmail.com
